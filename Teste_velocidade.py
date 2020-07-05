3a3eca39e949cc8acf0ef23f007d5c2153430c9d
evertoont <evertontbr@gmail.com>
2020-07-05 20:14:02 -0300

    Adiciona função teste_velocidade

--- Teste_velocidade.py
+++ Teste_velocidade.py
@@ -3,8 +3,26 @@ import speedtest
 velocidade = speedtest.Speedtest()
 
 
+def teste_velocidade(opcao):
+    print('\nRealizando teste de velocidade...')
+
+    servidores = []
+    velocidade.get_servers(servidores)
+    velocidades = [velocidade.download(), velocidade.upload(),
+                   velocidade.results.ping]
+
+    if opcao == 1:
+        return (velocidades[0], 'Download')
+    elif opcao == 2:
+        return (velocidades[1], 'Upload')
+    elif opcao == 3:
+        return (velocidades[2], 'Ping')
+    elif opcao == 4:
+        return (velocidades, 'Todos')
+
+
 def main():
-    
+
     opcao = int(input('''\nQual teste de velocidade você gostaria de fazer:
     1) Download
     2) Upload
