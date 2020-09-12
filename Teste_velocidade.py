6cd7415f63d24f4aae6fa4d11d59bb8651f4a1b7
evertoont <evertoontcarmo@gmail.com>
2020-09-12 18:15:59 -0300

    Atualiza interacao com o usuario

--- Teste_velocidade.py
+++ Teste_velocidade.py
@@ -4,44 +4,37 @@ from tabulate import tabulate
 velocidade = speedtest.Speedtest()
 
 
-def teste_velocidade(opcao):
-    print('\nRealizando teste de velocidade...')
+def teste_velocidade():
+    print('\nRealizando teste de velocidade, porfavor aguarde... \n')
 
     servidores = []
     velocidade.get_servers(servidores)
-    velocidades = [str(f"{round(velocidade.download() / 1_000_000, 2)} Mbps"), str(f"{round(velocidade.upload() / 1_000_000, 2)} Mbps"),
+
+    velocidades = [str(f"{round(velocidade.download() / 1_000_000, 2)} Mbps"),
+                   str(f"{round(velocidade.upload() / 1_000_000, 2)} Mbps"),
                    str(f"{round(velocidade.results.ping, 2)} ms")]
 
-    if opcao == 1:
-        return (velocidades[0], 'Download')
-    elif opcao == 2:
-        return (velocidades[1], 'Upload')
-    elif opcao == 3:
-        return (velocidades[2], 'Ping')
-    elif opcao == 4:
-        return (velocidades, 'Todos')
+    return velocidades
 
 
 def main():
 
-    opcao = int(input('''\nQual teste de velocidade você gostaria de fazer:
-    1) Download
-    2) Upload
-    3) Ping
-    4) Todos
+    input('''
+    >>> Para obter um melhor resultado feche todos os programas do computador.
+    >>> Se possível reinicie o PC para desativar todos os programas que ficam rodando em segundo plano.
+    
+    >>> Pressione ENTER para iniciar o teste de velocidade <<<
+    ''')
 
-    Sua escolha: '''))
+    velocidade = teste_velocidade()
 
-    velocidade, tipo = teste_velocidade(opcao)
+    tabela = [["Download", "Upload", "Ping"], [
+        velocidade[0], velocidade[1], velocidade[2]]]
 
-    if opcao == 4:
-        tabela = [["Download", "Upload", "Ping"], [ velocidade[0], velocidade[1], velocidade[2]]]
-        tabela_velocidade = tabulate(tabela, headers="firstrow", tablefmt="fancy_grid")
+    tabela_velocidade = tabulate(
+        tabela, headers="firstrow", tablefmt="fancy_grid")
 
-        print(tabela_velocidade)
-
-    else:
-        print(f'\nSeu {tipo} é de {velocidade}\n')
+    print(tabela_velocidade)
 
 
 if __name__ == '__main__':
